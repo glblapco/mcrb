7da70ce2e86f9a1ebed8f7c063f71da6883589d7
Biel Polastrini <bielp07@gmail.com>
2022-04-11 19:39:27 -0300

    Start.

---
+++ src/main/java/xyz/blapco/bl/mcrb/McrbApplication.java
@@ -0,0 +1,13 @@
+package xyz.blapco.bl.mcrb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McrbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McrbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/blapco/bl/mcrb/McrbApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.blapco.bl.mcrb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McrbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
