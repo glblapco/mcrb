8d122429691c2cb6a320ccc34cf13101403535aa
Biel Polastrini <bielp07@gmail.com>
2022-04-15 17:25:40 -0300

    Create PublicationModel.

---
+++ src/main/java/xyz/blapco/bl/mcrb/model/PublicationModel.java
@@ -0,0 +1,31 @@
+package xyz.blapco.bl.mcrb.model;
+
+public class PublicationModel {
+    private String title;
+    private String pubBody;
+    private String date;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPubBody() {
+        return pubBody;
+    }
+
+    public void setPubBody(String pubBody) {
+        this.pubBody = pubBody;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
