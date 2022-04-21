0d1e3de3514dfc47d7de405295b8caa78473d929
Biel Polastrini <bielp07@gmail.com>
2022-04-21 14:25:22 -0300

    Create Publication Repository.
    Create PublicationRepository and add it as a dependency on PublicationController.

--- src/main/java/xyz/blapco/bl/mcrb/controller/PublicationController.java
+++ src/main/java/xyz/blapco/bl/mcrb/controller/PublicationController.java
@@ -1,10 +1,14 @@
 package xyz.blapco.bl.mcrb.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import xyz.blapco.bl.mcrb.repository.PublicationRepository;
 
 @Controller
 public class PublicationController {
+    @Autowired
+    private PublicationRepository pr;
     @RequestMapping("publication/create")
     public String createPublication() {
         return "publication/create";
---
+++ src/main/java/xyz/blapco/bl/mcrb/repository/PublicationRepository.java
@@ -0,0 +1,7 @@
+package xyz.blapco.bl.mcrb.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import xyz.blapco.bl.mcrb.model.Publication;
+
+public interface PublicationRepository extends CrudRepository<Publication, String> {
+}
