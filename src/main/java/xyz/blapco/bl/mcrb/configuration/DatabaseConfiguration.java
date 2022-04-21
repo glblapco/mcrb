5278ef80932f061b0ba3902cd919da131793d888
Biel Polastrini <bielp07@gmail.com>
2022-04-21 14:22:37 -0300

    Finish DatabaseConfiguration.java

--- src/main/java/xyz/blapco/bl/mcrb/configuration/DatabaseConfiguration.java
+++ src/main/java/xyz/blapco/bl/mcrb/configuration/DatabaseConfiguration.java
@@ -4,13 +4,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @Configuration
 @Profile("develop")
 public class DatabaseConfiguration {
     public static void connectDB() {
         Connection connection = null;
-
-        String dbUrl = "jdbc:sqlite:mcrb.db";
+        try {
+            String dbUrl = "jdbc:sqlite:mcrb.db";
+            connection = DriverManager.getConnection(dbUrl);
+        } catch (SQLException sqlException) {
+            System.out.println(sqlException.getMessage());
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
     }
 }
