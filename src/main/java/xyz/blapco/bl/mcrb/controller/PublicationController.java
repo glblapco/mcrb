02434f882e3829fd5d5f01bf3991e5ee1a7cea52
Biel Polastrini <bielp07@gmail.com>
2022-04-23 21:17:06 -0300

    Store publication on database.
    Get publication from '/mcrb/publication/create' and store it on the database with an auto generated/incremental ID.

--- src/main/java/xyz/blapco/bl/mcrb/controller/PublicationController.java
+++ src/main/java/xyz/blapco/bl/mcrb/controller/PublicationController.java
@@ -3,14 +3,24 @@ package xyz.blapco.bl.mcrb.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import xyz.blapco.bl.mcrb.model.Publication;
 import xyz.blapco.bl.mcrb.repository.PublicationRepository;
 
 @Controller
 public class PublicationController {
+//@@@ TODO (Biel Polastrini): Get current datetime before storing the data.
     @Autowired
-    private PublicationRepository pr;
-    @RequestMapping("publication/create")
-    public String createPublication() {
+    private PublicationRepository publicationRepository;
+    @RequestMapping(value = "publication/create", method = RequestMethod.GET)
+    public String getPublication() {
+
         return "publication/create";
+    }
+
+    @RequestMapping(value = "publication/create", method = RequestMethod.POST)
+    public String createPublication(Publication publication) {
+        publicationRepository.save(publication);
+        return "redirect:/publication/create";
     }
 }
