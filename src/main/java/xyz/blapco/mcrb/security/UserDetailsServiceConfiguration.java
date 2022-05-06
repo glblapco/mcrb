73e63b1e7d96acc013e1a083fd4cbcf5b7f735ff
Biel Polastrini <bielp07@gmail.com>
2022-05-06 13:11:41 -0300

    Forgot the @Repository.

--- src/main/java/xyz/blapco/mcrb/security/UserDetailsServiceConfiguration.java
+++ src/main/java/xyz/blapco/mcrb/security/UserDetailsServiceConfiguration.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Repository;
 import xyz.blapco.mcrb.model.User;
 import xyz.blapco.mcrb.repository.UserRepository;
 
+@Repository
 public class UserDetailsServiceConfiguration implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
